faf94e2844ae5fb216ecd7940ba63bc41676b0ed
Jose Mosquera <jose-14mosquera@hotmail.com>
2024-07-17 22:27:28 -0500

    Agregar archivos de proyecto.

---
+++ operadores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Escribir un programa que declare variables para almacenar el nombre, 
+             * la edad y la estatura de una persona. 
+             * Asignar valores a las variables y luego mostrarlos en la consola.
+             */
+            string Nombre = "Jose Mosquera";
+            int Edad = 21;
+            double Estatura = 1.50;
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Tu edad es : " + Edad);
+            Console.WriteLine("Tu altura:" + Estatura);
+
+            Thread.Sleep(5000);
+            Console.WriteLine("Pulse una tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ operadoresej2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace operadoresej2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero entero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro numero");
+            int num2 = int.Parse(Console.ReadLine());
+            if (num1 < num2)
+            {
+                Console.WriteLine("el menor es " + num1);
+                Console.WriteLine("el mayor es " + num2);
+            }
+            else if (num1 > num2)
+            {
+                Console.WriteLine("el menor es " + num2);
+                Console.WriteLine("el mayor es " + num1);
+            }
+            else
+            {
+                Console.WriteLine(num2 + " ambos son iguales " + num1);
+            }
+            Thread.Sleep(500);
+            Console.WriteLine("pulse para continuar");
+            Console.ReadKey();
+        }
+    }
+}
