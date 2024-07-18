324d6736fd89e4d2afdab30de553ecef1878cdaf
Jose Mosquera <jose-14mosquera@hotmail.com>
2024-07-17 22:35:40 -0500

    poperadoresej4 y operadoresej5 añadidos

---
+++ operadoresej4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operadoresej4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la longitud de un lado del cuadrado: ");
+            double lado = Convert.ToDouble(Console.ReadLine());
+
+            double area = CalcularArea(lado);
+            double perimetro = CalcularPerimetro(lado);
+
+            Console.WriteLine($"El área del cuadrado es: {area} m²");
+            Console.WriteLine($"El perímetro del cuadrado es: {perimetro} m");
+
+            Console.ReadKey();
+        }
+
+        static double CalcularArea(double lado)
+        {
+            return lado * lado;
+        }
+
+        static double CalcularPerimetro(double lado)
+        {
+            return 4 * lado;
+        }
+    }
+ }
---
+++ operadoresej5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operadoresej5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el radio de la esfera: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            double area = CalcularArea(radio);
+            double volumen = CalcularVolumen(radio);
+
+            Console.WriteLine($"El área de la esfera es: {area} u²");
+            Console.WriteLine($"El volumen de la esfera es: {volumen} u³");
+
+            Console.ReadKey();
+        }
+
+        static double CalcularArea(double radio)
+        {
+            return 4 * Math.PI * Math.Pow(radio, 2);
+        }
+
+        static double CalcularVolumen(double radio)
+        {
+            return (4 / 3) * Math.PI * Math.Pow(radio, 3);
+        
+        }
+    }
+}
